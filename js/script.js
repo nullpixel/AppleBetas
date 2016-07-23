b65271ccc3efdc273213795f7e52d9c73b91d50f
BuildTools <applebetas@icloud.com>
2016-07-22 21:32:45 -0400

    Fix js typo

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
         'wholeDelay': startText.length * 100
     });
     $(".intro.end").typewrite({
-        'wholeDelay': (startText.length nameText.length) * 100,
+        'wholeDelay': (startText.length + nameText.length) * 100,
         'callback': function() {
             $(".links").animate({opacity: 1}, 250, function() {
                 var count = 0
